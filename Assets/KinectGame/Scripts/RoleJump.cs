7b7e35182c0f4f27e46e3d4caf6af1f781214211
Zhuo <zhchen@nreal.ai>
2021-01-28 11:03:32 +0800

    update name

--- Assets/KinectGame/Scripts/RoleJump.cs
+++ Assets/KinectGame/Scripts/RoleJump.cs
@@ -7,22 +7,20 @@ public class RoleJump : MonoBehaviour
     float defalutY = 0;
     float emulatorG = -9.8f;
     float emulatorY = 3;
-    float curUp;
+    float upward;
     bool jump = true;
     public void Init()
     {
         defalutY = transform.position.y;
-        curUp = emulatorY;
+        upward = emulatorY;
         jump = true;
     }
 
     private void FixedUpdate()
     {
         if (!jump) return;
-        curUp +=  emulatorG * Time.fixedDeltaTime;
-        Debug.Log(curUp);
-        //transform.position = new Vector3(transform.position.x, transform.position.y + curUp * Time.fixedDeltaTime, transform.position.z);
-        transform.position += Vector3.up * curUp * Time.fixedDeltaTime;
+        upward +=  emulatorG * Time.fixedDeltaTime;
+        transform.position += Vector3.up * upward * Time.fixedDeltaTime;
         if (transform.position.y < defalutY)
         {
             transform.position = new Vector3(transform.position.x, defalutY, transform.position.z);
